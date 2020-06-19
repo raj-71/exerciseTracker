81fb1c4e1c79d17b6d4b5c8e84da8387da5d81af
raj-71 <40698372+raj-71@users.noreply.github.com>
2020-06-19 16:16:45 +0530

    Add files via upload

---
+++ client/build/precache-manifest.b5367a1ce9221c5ecf622d92f622c3a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b389452c8b571a9ac279d8a859ab7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6834ff03648875a083",
+    "url": "/static/css/2.6d92adba.chunk.css"
+  },
+  {
+    "revision": "a6148628db3588050e67",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e6834ff03648875a083",
+    "url": "/static/js/2.a6cae164.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.a6cae164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6148628db3588050e67",
+    "url": "/static/js/main.8906a35f.chunk.js"
+  },
+  {
+    "revision": "d8b62c80f1c59c21062b",
+    "url": "/static/js/runtime-main.9a98116c.js"
+  }
+]);
